4174e39f2bf605bd10b8d8f09339b9c49d4f5f30
szaimen <szaimen@e.mail.de>
2021-12-07 18:35:18 +0100

    run image prune and volume prune regularly
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/mastercontainer/cron.sh
+++ Containers/mastercontainer/cron.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-set -eu
+set -eux
 
 while true; do
     sudo -u www-data php /var/www/docker-aio/php/src/Cron/cron.php
+    sudo -u www-data docker image prune -f
+    sudo -u www-data docker volume prune -f
     sleep 1d
 done
