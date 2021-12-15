3206ce4d16229ade6a3a5f825711432516789850
szaimen <szaimen@e.mail.de>
2021-12-15 13:07:39 +0100

    remove volume prune as we don't want to risk anything
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/mastercontainer/cron.sh
+++ Containers/mastercontainer/cron.sh
@@ -6,12 +6,5 @@ while true; do
     sudo -u www-data php /var/www/docker-aio/php/src/Cron/cron.php
     # Remove dangling images
     sudo -u www-data docker image prune -f
-    # Remove dangling volumes
-    unset DANGLING_VOLUMES
-    DANGLING_VOLUMES="$(sudo -u www-data docker volume ls --filter dangling=true | awk '{print $2}' | grep -v "nextcloud_aio_\|^VOLUME$")"
-    mapfile -t DANGLING_VOLUMES <<< "$DANGLING_VOLUMES"
-    for volume in "${DANGLING_VOLUMES[@]}"; do
-        sudo -u www-data docker volume rm "$volume"
-    done
     sleep 1d
 done
