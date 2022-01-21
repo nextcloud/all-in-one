6b6c9dfbc7d13318aa9d9c6be890e98fb8db22c0
szaimen <szaimen@e.mail.de>
2022-01-21 18:29:13 +0100

    Correctly set CPU_ARCH for notify_push
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/nextcloud/start.sh
+++ Containers/nextcloud/start.sh
@@ -11,4 +11,13 @@ if ! bash /entrypoint.sh; then
     exit 1
 fi
 
+# Correctly set CPU_ARCH for notify_push
+export CPU_ARCH="$(uname -p)"
+if [ -z "$CPU_ARCH" ]; then
+    echo "Could not get processor architecture. Exiting."
+    exit 1
+elif [ "$CPU_ARCH" != "x86_64" ]; then
+    export CPU_ARCH="aarch64"
+fi
+
 exec "$@"
