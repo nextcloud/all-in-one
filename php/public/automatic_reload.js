4c304d87750a00303b9e98914b693684334366e7
szaimen <szaimen@e.mail.de>
2022-03-11 17:00:24 +0100

    move inline javascript to its own file
    Signed-off-by: szaimen <szaimen@e.mail.de>

---
+++ php/public/automatic_reload.js
@@ -0,0 +1,13 @@
+if (document.hasFocus()) {
+    // hide reload button if the site reloads automatically
+    var list = document.getElementsByClassName("reload button");
+    for (var i = 0; i < list.length; i++) {
+        // list[i] is a node with the desired class name
+        list[i].style.display = 'none';
+    }
+
+    // set timeout for reload
+    setTimeout(function(){
+    window.location.reload(1);
+    }, 5000);
+}
