ea098a4d1aa4d10b30e2c108f3ccf12d1f198ddc
szaimen <szaimen@e.mail.de>
2022-03-16 22:28:17 +0100

    wait for clamav to become available
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/nextcloud/entrypoint.sh
+++ Containers/nextcloud/entrypoint.sh
@@ -306,6 +306,10 @@ php /var/www/html/occ config:app:set spreed signaling_servers --value="$SIGNALIN
 
 # Clamav
 if [ "$CLAMAV_ENABLED" = 'yes' ]; then
+    while ! nc -z "$CLAMAV_HOST" 3310; do
+        echo "waiting for clamav to become available..."
+        sleep 5
+    done
     if ! [ -d "/var/www/html/custom_apps/files_antivirus" ]; then
         php /var/www/html/occ app:install files_antivirus
     elif [ "$(php /var/www/html/occ config:app:get files_antivirus enabled)" = "no" ]; then
