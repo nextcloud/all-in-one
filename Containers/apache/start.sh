1f6b8d48a90c4c67906af5b42162dba1772a74b6
szaimen <szaimen@e.mail.de>
2022-05-29 16:25:00 +0200

    fix writing to the caddyfile
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/apache/start.sh
+++ Containers/apache/start.sh
@@ -21,14 +21,16 @@ if [ -z "$APACHE_PORT" ]; then
     export APACHE_PORT="443"
 fi
 
+# Change the Caddyfile
 if [ "$APACHE_PORT" != '443' ]; then
     export PROTOCOL="http"
     export NC_DOMAIN=""
-    sed -i 's|auto_https.*|auto_https off|' /Caddyfile
+    CADDYFILE="$(sed 's|auto_https.*|auto_https off|' /Caddyfile)"
 else
     export PROTOCOL="https"
-    sed -i 's|auto_https.*|auto_https disable_redirects|' /Caddyfile
+    CADDYFILE="$(sed 's|auto_https.*|auto_https disable_redirects|' /Caddyfile)"
 fi
+echo "$CADDYFILE" > /Caddyfile
 
 # Add caddy path
 mkdir -p /mnt/data/caddy/
