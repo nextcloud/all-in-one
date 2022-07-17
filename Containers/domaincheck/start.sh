cd6e0ed9d46c0317d436091fdf1bb552cc81772a
szaimen <szaimen@e.mail.de>
2022-07-17 19:30:34 +0200

    fix the lighttpd config
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/domaincheck/start.sh
+++ Containers/domaincheck/start.sh
@@ -11,6 +11,9 @@ if [ -z "$APACHE_PORT" ]; then
     export APACHE_PORT="443"
 fi
 
+CONF_FILE="$(sed "s|ipv6-placeholder|\[::\]:$APACHE_PORT|" /etc/lighttpd/lighttpd.conf)"
+echo "$CONF_FILE" > /etc/lighttpd/lighttpd.conf
+
 # Check config file
 lighttpd -tt -f /etc/lighttpd/lighttpd.conf
 
