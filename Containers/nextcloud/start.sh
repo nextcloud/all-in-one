4302b7abbd7c66b9a0abe813487ccfa15e0fba1c
Lorenzo Marroccoli <lollo0296@gmail.com>
2022-08-04 11:11:13 +0200

    debug echos
    Signed-off-by: Lorenzo Marroccoli <lollo0296@gmail.com>

--- Containers/nextcloud/start.sh
+++ Containers/nextcloud/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "start.sh - Start"
 # Only start container if database is accessible
 while ! nc -z "$POSTGRES_HOST" 5432; do
     echo "Waiting for database to start..."
@@ -15,18 +15,18 @@ if [ -f "/var/www/html/config/config.php" ] && sleep 2 && psql -d "postgresql://
     sed -i "s|'dbuser'.*=>.*$|'dbuser' => '$POSTGRES_USER',|" /var/www/html/config/config.php
     sed -i "s|'dbpassword'.*=>.*$|'dbpassword' => '$POSTGRES_PASSWORD',|" /var/www/html/config/config.php
 fi
-
+echo "start.sh - Before entrypoint.sh"
 # Run original entrypoint
 if ! bash /entrypoint.sh; then
     exit 1
 fi
-
+echo "start.sh - After entrypoint.sh"
 # Trust additional Cacerts, if needed
 if [ -n "$TRUSTED_CACERTS_DIR" ]; then
     echo "User required to trust additional CA certificates, doing so now."
     update-ca-certificates
 fi
-
+echo "start.sh - After update-ca-certificates"
 # Correctly set CPU_ARCH for notify_push
 CPU_ARCH="$(uname -m)"
 export CPU_ARCH
@@ -36,5 +36,5 @@ if [ -z "$CPU_ARCH" ]; then
 elif [ "$CPU_ARCH" != "x86_64" ]; then
     export CPU_ARCH="aarch64"
 fi
-
+echo "start.sh - End"
 exec "$@"
