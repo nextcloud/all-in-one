f8017a0a021831e149c0b3967d0b15bd8747eb67
szaimen <szaimen@e.mail.de>
2022-08-26 13:59:04 +0200

    fix FTS
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/nextcloud/activate-fulltextsearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$FULLTEXTSEARCH_ENABLED" != yes ]; then
-    # Basically sleep for forever if fulltextsearch is not enabled
-    sleep inf
-fi
-echo "Activating fulltextsearch..."
-php /var/www/html/occ fulltextsearch:live -q
-sleep inf
