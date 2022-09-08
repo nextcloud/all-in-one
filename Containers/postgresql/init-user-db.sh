feb6438d39424b9f37d6c4a71952115c5bce4e30
Simon L <szaimen@e.mail.de>
2022-09-08 17:26:36 +0200

    Revert "add a check for init-user-db.sh"
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/postgresql/init-user-db.sh
+++ Containers/postgresql/init-user-db.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -ex
 
-touch /mnt/data/initdb.failed
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER "oc_$POSTGRES_USER" WITH PASSWORD '$POSTGRES_PASSWORD' CREATEDB;
 	ALTER DATABASE "$POSTGRES_DB" OWNER TO "oc_$POSTGRES_USER";
 EOSQL
-
-rm /mnt/data/initdb.failed
 
 set +ex
