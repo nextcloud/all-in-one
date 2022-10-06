95311fd7c3887a7e183b61e63212316f05da75c8
szaimen <szaimen@e.mail.de>
2022-10-06 22:34:10 +0200

    use netcat for healthchecks
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- Containers/apache/healthcheck.sh
+++ Containers/apache/healthcheck.sh
@@ -2,7 +2,7 @@
 
 curl -skfI localhost:8000 || exit 1
 if [ "$APACHE_PORT" != '443' ]; then
-    curl -skfI localhost:"$APACHE_PORT" || exit 1
+    nc -z localhost "$APACHE_PORT" || exit 1
 else
-    curl -skfI https://"$NC_DOMAIN":"$APACHE_PORT" || exit 1
+    nc -z "$NC_DOMAIN" "$APACHE_PORT" || exit 1
 fi
