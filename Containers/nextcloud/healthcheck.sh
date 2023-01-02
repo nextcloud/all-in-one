ed5483500aa04d528ceb6ace165efe3bbbde765d
Simon L <szaimen@e.mail.de>
2023-01-02 18:24:17 +0100

    Address review
    Signed-off-by: Simon L <szaimen@e.mail.de>
    Co-Authored-By: Zoey <zoey@z0ey.de>

--- Containers/nextcloud/healthcheck.sh
+++ Containers/nextcloud/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo -u www-data nc -z "$POSTGRES_HOST" 5432 || exit 0
+nc -z "$POSTGRES_HOST" 5432 || exit 0
 
-if ! sudo -u www-data nc -z localhost 9000 || ! sudo -u www-data nc -z localhost 7867; then
+if ! nc -z localhost 9000 || ! nc -z localhost 7867; then
     exit 1
 fi
