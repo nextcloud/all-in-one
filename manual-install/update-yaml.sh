3cf99a96b2ebeddfcffab4c64940dc28ae99c961
Simon L <szaimen@e.mail.de>
2023-01-05 19:20:58 +0100

    add verbose output for update-yaml
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- manual-install/update-yaml.sh
+++ manual-install/update-yaml.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 jq -c . ./php/containers.json > /tmp/containers.json
 sed -i 's|aio_services_v1|services|g' /tmp/containers.json
 sed -i 's|","destination":"|:|g' /tmp/containers.json
