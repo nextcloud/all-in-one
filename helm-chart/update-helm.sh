baacc224bff4cf88045253e611da2ba26d2dd3bd
Simon L <szaimen@e.mail.de>
2023-01-05 20:11:20 +0100

    WIP

--- helm-chart/update-helm.sh
+++ helm-chart/update-helm.sh
@@ -20,7 +20,7 @@ cp sample.conf /tmp/
 sed -i 's|^|export |' /tmp/sample.conf
 source /tmp/sample.conf
 rm /tmp/sample.conf
-sed -i "s|\${APACHE_IP_BINDING}|127.0.0.1|" latest.yml
+sed -i "s|\${APACHE_IP_BINDING}|$APACHE_IP_BINDING|" latest.yml
 sed -i "s|\${APACHE_PORT}:\${APACHE_PORT}/|$APACHE_PORT:$APACHE_PORT/|" latest.yml
 sed -i "s|\${TALK_PORT}:\${TALK_PORT}/|$TALK_PORT:$TALK_PORT/|g" latest.yml
 sed -i "s|\${NEXTCLOUD_DATADIR}|$NEXTCLOUD_DATADIR|" latest.yml
@@ -29,6 +29,7 @@ sed -i "s|\${NEXTCLOUD_TRUSTED_CACERTS_DIR}|nextcloud_aio_nextcloud_trusted_cace
 sed -i 's|\${|{{ .Values.|g' latest.yml
 sed -i 's|}| }}|g' latest.yml
 sed -i '/profiles: /d' latest.yml
+sed -i 's|storage: 100Mi|storage: {{ .Values.MAX_STORAGE_SIZE }}|g' latest.yml
 cat latest.yml
 kompose convert -c -f latest.yml
 cd latest
@@ -38,6 +39,7 @@ find ./ -name '*apache*' -exec sed -i "s|$APACHE_IP_BINDING|{{ .Values.APACHE_IP
 find ./ -name '*apache*' -exec sed -i "s|$APACHE_PORT|{{ .Values.APACHE_PORT }}|" \{} \;  
 find ./ -name '*talk*' -exec sed -i "s|$TALK_PORT|{{ .Values.TALK_PORT }}|" \{} \; 
 find ./ -name '*.yaml' -exec sed -i "s|'{{|\"{{|g;s|}}'|}}\"|g" \{} \; 
+find ./ -name '*.yaml' -exec sed -i "/status: {}$/d" \{} \; 
 cd ../
 mkdir -p ../helm-chart/
 rm latest/README.md
@@ -48,12 +50,14 @@ mv latest.yml.backup latest.yml
 
 # Conversion of sample.conf
 cp sample.conf /tmp/
+sed -i "/^APACHE_IP_BINDING/d" /tmp/sample.conf
 sed -i 's|"||g' /tmp/sample.conf
 sed -i 's|= |: "" |' /tmp/sample.conf
 sed -i 's|=|: |' /tmp/sample.conf
-sed -i '/^\$NEXTCLOUD_DATADIR/d' /tmp/sample.conf
-sed -i '/^\$NEXTCLOUD_MOUNT/d' /tmp/sample.conf
-sed -i '/^\$NEXTCLOUD_TRUSTED_CACERTS_DIR/d' /tmp/sample.conf
+sed -i '/^NEXTCLOUD_DATADIR/d' /tmp/sample.conf
+sed -i '/^NEXTCLOUD_MOUNT/d' /tmp/sample.conf
+sed -i '/^NEXTCLOUD_TRUSTED_CACERTS_DIR/d' /tmp/sample.conf
+echo 'MAX_STORAGE_SIZE: 10Ti' >> /tmp/sample.conf
 mv /tmp/sample.conf ../helm-chart/values.yaml
 
 chmod 777 -R ../helm-chart/
