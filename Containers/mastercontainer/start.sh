4e74052c20171c4877d05f1ff64068f49809e881
Zoey <zoey@z0ey.de>
2023-01-27 21:36:53 +0100

    migrate mastercontainer to alpine (#1577)

--- Containers/mastercontainer/start.sh
+++ Containers/mastercontainer/start.sh
@@ -239,8 +239,8 @@ chown root:root -R /mnt/docker-aio-config/certs/
 
 # Don't allow access to the AIO interface from the Nextcloud container
 # Probably more cosmetic than anything but at least an attempt
-if ! grep -q '# nextcloud-aio-block' /etc/apache2/apache2.conf; then
-    cat << APACHE_CONF >> /etc/apache2/apache2.conf
+if ! grep -q '# nextcloud-aio-block' /etc/apache2/httpd.conf; then
+    cat << APACHE_CONF >> /etc/apache2/httpd.conf
 # nextcloud-aio-block-start
 <Location />
 order allow,deny
@@ -276,5 +276,8 @@ https://your-domain-that-points-to-this-server.tld:8443"
 
 # Set the timezone to UTC
 export TZ=UTC
+
+# Fix apache startup
+rm -f /var/run/apache2/httpd.pid
 
 exec "$@"
