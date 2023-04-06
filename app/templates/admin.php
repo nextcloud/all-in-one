157588d95f48307af3c8f25e574c0042c0349b1c
Simon L <szaimen@e.mail.de>
2023-04-07 01:58:09 +0200

    add a link to the easy-login docs to the app
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- app/templates/admin.php
+++ app/templates/admin.php
@@ -11,5 +11,6 @@ declare(strict_types=1);
 	/** @var array $_ */ ?>
 <div id="allinone" class="section">
 	<h2><?php p($l->t('Nextcloud All In One'));?></h2>
-	<a href="<?php p($_['AIOLoginUrl']);?>" class="button" target="_blank" rel="noopener">Open Nextcloud AIO Interface ↗</a>
+	<a href="<?php p($_['AIOLoginUrl']);?>" class="button" target="_blank" rel="noopener">Open Nextcloud AIO Interface ↗</a><br>
+	<p><a href="https://github.com/nextcloud/all-in-one#how-to-easily-log-in-to-the-aio-interface">Click here for more infos on this feature (e.g. also on how to change the link in the button)</a></p>
 </div>
