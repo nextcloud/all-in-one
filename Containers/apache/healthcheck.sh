d93c01f3686c3b977ce55f9aaa4289274cb828ab
Zoey <zoey@z0ey.de>
2023-04-08 13:28:19 +0200

    apache healthcheck, check localhost apacheport with every apache port
    Signed-off-by: Zoey <zoey@z0ey.de>

--- Containers/apache/healthcheck.sh
+++ Containers/apache/healthcheck.sh
@@ -2,8 +2,7 @@
 
 nc -z "$NEXTCLOUD_HOST" 9000 || exit 0
 nc -z localhost 8000 || exit 1
-if [ "$APACHE_PORT" != '443' ]; then
-    nc -z localhost "$APACHE_PORT" || exit 1
-else
+nc -z localhost "$APACHE_PORT" || exit 1
+if [ "$APACHE_PORT" == '443' ]; then
     nc -z "$NC_DOMAIN" "$APACHE_PORT" || exit 1
 fi
