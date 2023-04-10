46ec1ea7f59e56c81b015d31b7d7dca2bd6997cc
Zoey <zoey@z0ey.de>
2023-04-10 17:50:58 +0200

    always check port 443
    Signed-off-by: Zoey <zoey@z0ey.de>

--- Containers/apache/healthcheck.sh
+++ Containers/apache/healthcheck.sh
@@ -3,6 +3,4 @@
 nc -z "$NEXTCLOUD_HOST" 9000 || exit 0
 nc -z localhost 8000 || exit 1
 nc -z localhost "$APACHE_PORT" || exit 1
-if [ "$APACHE_PORT" == '443' ]; then
-    nc -z "$NC_DOMAIN" "$APACHE_PORT" || exit 1
-fi
+nc -z "$NC_DOMAIN" "$APACHE_PORT" || exit 1
