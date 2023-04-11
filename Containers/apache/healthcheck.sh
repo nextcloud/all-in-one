904d23147c594ca6a25e35b42aa6bee66331e5ae
Zoey <zoey@z0ey.de>
2023-04-11 10:54:02 +0200

    add suggestion
    Co-authored-by: Simon L. <szaimen@e.mail.de>
    Signed-off-by: Zoey <zoey@z0ey.de>

--- Containers/apache/healthcheck.sh
+++ Containers/apache/healthcheck.sh
@@ -3,4 +3,4 @@
 nc -z "$NEXTCLOUD_HOST" 9000 || exit 0
 nc -z localhost 8000 || exit 1
 nc -z localhost "$APACHE_PORT" || exit 1
-nc -z "$NC_DOMAIN" "$APACHE_PORT" || exit 1
+nc -z "$NC_DOMAIN" 443 || exit 1
