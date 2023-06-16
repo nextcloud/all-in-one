1c595ab2ef597f33fc0c3616cfeedfa1512ea2fa
Simon L <szaimen@e.mail.de>
2023-06-16 23:55:43 +0200

    add empty line before storage class in helm-chart
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- nextcloud-aio-helm-chart/update-helm.sh
+++ nextcloud-aio-helm-chart/update-helm.sh
@@ -193,6 +193,7 @@ sed -i '/_ENABLED.*/s/ yes / "yes" /' /tmp/sample.conf
 sed -i '/_ENABLED.*/s/ no / "no" /' /tmp/sample.conf
 sed -i 's|^NEXTCLOUD_TRUSTED_CACERTS_DIR: .*|NEXTCLOUD_TRUSTED_CACERTS_DIR:        # Setting this to any value allows to automatically import root certificates into the Nextcloud container|' /tmp/sample.conf
 sed -i 's|10737418240|"10737418240"|' /tmp/sample.conf
+echo "" >> /tmp/sample.conf
 # shellcheck disable=SC2129
 echo 'STORAGE_CLASS:        # By setting this, you can adjust the storage class for your volumes' >> /tmp/sample.conf
 for variable in "${VOLUME_VARIABLE[@]}"; do
