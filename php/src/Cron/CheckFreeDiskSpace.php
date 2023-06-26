2b5d3de48e281c10138924015a150fae25604dc8
Simon L <szaimen@e.mail.de>
2023-06-26 14:48:02 +0200

    adjust wording
    Signed-off-by: Simon L <szaimen@e.mail.de>
    
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- php/src/Cron/CheckFreeDiskSpace.php
+++ php/src/Cron/CheckFreeDiskSpace.php
@@ -21,6 +21,6 @@ $nextcloudContainer = $containerDefinitionFetcher->GetContainerById($id);
 
 $df = disk_free_space(DataConst::GetDataDirectory());
 if ($df !== false && (int)$df < 1024 * 1024 * 1024 * 5) {
-    error_log("The drive that hosts all docker volumes has less than 5 GB free space. Container updates and backups might not succeed due to that!");
-    $dockerActionManger->sendNotification($nextcloudContainer, 'Low on space!', 'The drive that hosts all docker volumes has less than 5 GB free space. Container updates and backups might not succeed due to that!');
+    error_log("The drive that hosts the mastercontainer volume has less than 5 GB free space. Container updates and backups might not succeed due to that!");
+    $dockerActionManger->sendNotification($nextcloudContainer, 'Low on space!', 'The drive that hosts the mastercontainer volume has less than 5 GB free space. Container updates and backups might not succeed due to that!');
 }
