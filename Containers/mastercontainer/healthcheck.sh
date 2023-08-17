d8e802851d0cca35873cd2284e6941b01694e557
Simon L <szaimen@e.mail.de>
2023-08-17 14:15:04 +0200

    mastercontainer - improve healthcheck
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- Containers/mastercontainer/healthcheck.sh
+++ Containers/mastercontainer/healthcheck.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 if [ -f "/mnt/docker-aio-config/data/configuration.json" ]; then
+    nc -z localhost 80 || exit 1
+    nc -z localhost 8000 || exit 1
     nc -z localhost 8080 || exit 1
+    nc -z localhost 8443 || exit 1
+    nc -z localhost 9000 || exit 1
+    nc -z localhost 9876 || exit 1
 fi
