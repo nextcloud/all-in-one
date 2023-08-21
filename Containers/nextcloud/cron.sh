14e8996e9060da50fd6bd438b1310b21a7a6ea79
Simon L <szaimen@e.mail.de>
2023-08-21 12:38:58 +0200

    nextcloud - add stop_grace_period of 600s
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- Containers/nextcloud/cron.sh
+++ Containers/nextcloud/cron.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -eu
 
+wait_for_cron() {
+    set -x
+    while [ -n "$(pgrep -f /var/www/html/cron.php)" ]; do
+        echo "Waiting for cron to stop..."
+        sleep 5
+    done
+    echo "Cronjob successfully exited."
+    set +x
+}
+
+trap wait_for_cron SIGINT SIGTERM
+
 while true; do
     php -f /var/www/html/cron.php &
-    sleep 5m
+    sleep 5m &
+    wait $!
 done
