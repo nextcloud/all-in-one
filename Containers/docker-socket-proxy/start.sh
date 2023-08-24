685786c5f6e9b2b6a21f210d2f72ab97ffe53851
Zoey <zoey@z0ey.de>
2023-08-24 16:46:35 +0200

    Update start.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- Containers/docker-socket-proxy/start.sh
+++ Containers/docker-socket-proxy/start.sh
@@ -3,6 +3,12 @@
 openssl req -nodes -new -x509 -subj '/CN=*' -sha256 -keyout /etc/privkey.pem -out /etc/fullchain.pem -days 365000
 cat /etc/fullchain.pem /etc/privkey.pem | tee /etc/cert.pem
 
+# Only start container if nextcloud is accessible
+while ! nc -z "$NEXTCLOUD_HOST" 9000; do
+    echo "Waiting for Nextcloud to start..."
+    sleep 5
+done
+
 set -x
 IPv4_ADDRESS_NC="$(dig nextcloud-aio-nextcloud IN A +short | grep '^[0-9.]\+$' | sort | head -n1)"
 sed -i "s|NC_IPV4_PLACEHOLDER|$IPv4_ADDRESS_NC|g"
