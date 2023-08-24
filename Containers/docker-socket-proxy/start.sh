0fbd7768e0644de0f4751263eba58967875afa66
Zoey <zoey@z0ey.de>
2023-08-24 21:02:49 +0200

    Update start.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- Containers/docker-socket-proxy/start.sh
+++ Containers/docker-socket-proxy/start.sh
@@ -9,11 +9,11 @@ done
 set -x
 IPv4_ADDRESS_NC="$(dig nextcloud-aio-nextcloud IN A +short | grep '^[0-9.]\+$' | sort | head -n1)"
 sed -i "s|NC_IPV4_PLACEHOLDER|$IPv4_ADDRESS_NC|g"
-sed -i '/NC_IPV4_PLACEHOLDER/d' /conf/eturnal.yml
+sed -i "s# || { src NC_IPV4_PLACEHOLDER }##g" /conf/haproxy.cfg
 
 IPv6_ADDRESS_NC="$(dig nextcloud-aio-nextcloud AAAA +short | grep '^[0-9a-f:]\+$' | sort | head -n1)"
 sed -i "s|NC_IPV6_PLACEHOLDER|$IPv6_ADDRESS_NC|g"
-sed -i '/NC_IPV6_PLACEHOLDER/d' /conf/eturnal.yml
+sed -i "s# || { src NC_IPV6_PLACEHOLDER }##g" /conf/haproxy.cfg
 set +x
 
-haproxy -f /haproxy.cfg -db
+haproxy -f /conf/haproxy.cfg -db
