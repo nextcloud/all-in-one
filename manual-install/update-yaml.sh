e87b2dc3130c15f297829ca9142fe919ac5a3e29
Simon L <szaimen@e.mail.de>
2023-08-25 15:11:48 +0200

    try to fix it
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- manual-install/update-yaml.sh
+++ manual-install/update-yaml.sh
@@ -21,7 +21,7 @@ OUTPUT="$(echo "$OUTPUT" | jq 'del(.services[] | select(.container_name == "next
 OUTPUT="$(echo "$OUTPUT" | jq 'del(.services[] | select(.container_name == "nextcloud-aio-domaincheck"))')"
 OUTPUT="$(echo "$OUTPUT" | jq 'del(.services[] | select(.container_name == "nextcloud-aio-borgbackup"))')"
 OUTPUT="$(echo "$OUTPUT" | jq 'del(.services[] | select(.container_name == "nextcloud-aio-docker-socket-proxy"))')"
-OUTPUT="$(echo "$OUTPUT" | jq '.services[].depends_on |=  if contains(["nextcloud-aio-docker-socket-proxy"]) then del(.[index("nextcloud-aio-docker-socket-proxy")])')"
+OUTPUT="$(echo "$OUTPUT" | jq '.services[].depends_on |=  if contains(["nextcloud-aio-docker-socket-proxy"]) then del(.[index("nextcloud-aio-docker-socket-proxy")]) else . end')"
 OUTPUT="$(echo "$OUTPUT" | jq '.services[] |= if has("depends_on") then .depends_on |= map({ (.): { "condition": "service_started", "required": false } }) else . end' | jq '.services[] |= if has("depends_on") then .depends_on |= reduce .[] as $item ({}; . + $item) else . end')"
 
 snap install yq
