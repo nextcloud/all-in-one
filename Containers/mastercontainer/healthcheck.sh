b972c9961642220a271333e0ac8759a8ac25763a
Zoey <zoey@z0ey.de>
2023-09-16 11:22:41 +0200

    use php unix socket (3/3)
    Signed-off-by: Zoey <zoey@z0ey.de>

--- Containers/mastercontainer/healthcheck.sh
+++ Containers/mastercontainer/healthcheck.sh
@@ -5,6 +5,6 @@ if [ -f "/mnt/docker-aio-config/data/configuration.json" ]; then
     nc -z localhost 8000 || exit 1
     nc -z localhost 8080 || exit 1
     nc -z localhost 8443 || exit 1
-    nc -z localhost 9000 || exit 1
     nc -z localhost 9876 || exit 1
+    [ -f /var/run/php.sock ] || exit 1
 fi
