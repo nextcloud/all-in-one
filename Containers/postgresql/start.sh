00ec781b6822c810dc50093f66683d03cb4e19f8
Simon L <szaimen@e.mail.de>
2023-10-21 15:40:57 +0200

    postgresql - close idling sessions automatically
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- Containers/postgresql/start.sh
+++ Containers/postgresql/start.sh
@@ -155,9 +155,13 @@ if [ -f "/var/lib/postgresql/data/postgresql.conf" ]; then
         sed -i "s|^max_connections =.*|max_connections = $MAX_CONNECTIONS|" "/var/lib/postgresql/data/postgresql.conf"
     fi
 
-    # Modify conf
+    # Do not log checkpoints
     if grep -q "#log_checkpoints" /var/lib/postgresql/data/postgresql.conf; then
         sed -i 's|#log_checkpoints.*|log_checkpoints = off|' /var/lib/postgresql/data/postgresql.conf
+    fi
+    # Close idling connections automatically after 3s which does not seem to happen automatically so that we run into max_connections limits
+    if grep -q "#idle_session_timeout" /var/lib/postgresql/data/postgresql.conf; then
+        sed -i 's|#idle_session_timeout.*|idle_session_timeout = 3000|' /var/lib/postgresql/data/postgresql.conf
     fi
 fi
 
