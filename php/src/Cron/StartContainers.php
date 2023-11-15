cfeb8d872a9a90cd688df7f3593da826bc7bfa5c
Simon L <szaimen@e.mail.de>
2023-11-15 21:04:25 +0100

    log whole error messages
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- php/src/Cron/StartContainers.php
+++ php/src/Cron/StartContainers.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 // increase memory limit to 2GB
 ini_set('memory_limit', '2048M');
 
+// Log whole log messages
+ini_set('log_errors_max_len', '0');
+
 use DI\Container;
 
 require __DIR__ . '/../../vendor/autoload.php';
