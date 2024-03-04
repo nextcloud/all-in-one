fd3f6d90180e4fb4c329a4febeb3201a036539eb
Simon L <szaimen@e.mail.de>
2024-03-04 11:48:21 +0100

    secure imaginary with imaginary_key
    Signed-off-by: Simon L <szaimen@e.mail.de>

---
+++ Containers/imaginary/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -z "$IMAGINARY_SECRET" ]; then
+    imaginary -return-size -max-allowed-resolution 222.2 "$@"
+else
+    imaginary -return-size -max-allowed-resolution 222.2 -key "$IMAGINARY_SECRET" "$@"
+fi
