b1ad854de6de5282be68578fbc4c254f113070c4
Simon L. <szaimen@e.mail.de>
2024-05-17 14:03:59 +0200

    Revert changes to database container
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- Containers/postgresql/healthcheck.sh
+++ Containers/postgresql/healthcheck.sh
@@ -2,9 +2,4 @@
 
 test -f "/mnt/data/backup-is-running" && exit 0
 
-# Set a default value for POSTGRES_PORT
-if [ -z "$POSTGRES_PORT" ]; then
-    POSTGRES_PORT=5432
-fi
-
-psql -d "postgresql://oc_$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:$POSTGRES_PORT/$POSTGRES_DB" -c "select now()" || exit 1
+psql -d "postgresql://oc_$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DB" -c "select now()" || exit 1
