eeccfec0c960117ba92e4276267d5bf328d5dc23
Simon L. <szaimen@e.mail.de>
2024-05-22 14:49:24 +0200

    Revert "pre-fill the domain"
    This reverts commit 46b6992b52a24ea5ef3f2370f1a9a9afff8556d0.
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- php/public/domain-prefill.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // domain
-    let domain = document.getElementById("domain_input");
-    if (domain) {
-        domain.value = window.location.host
-    }
-});
