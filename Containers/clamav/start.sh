f72ab28a4172f28f3095ead9b39308bef1c3f9d7
Simon L. <szaimen@e.mail.de>
2024-07-11 15:54:25 +0200

    fix clamav
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/clamav/start.sh
+++ Containers/clamav/start.sh
@@ -6,7 +6,6 @@ CLAMAV_FILE="$(sed "s|10G|$MAX_SIZE|" /clamav.conf)"
 echo "$CLAMAV_FILE" >> /tmp/clamd.conf
 
 # Call initial init
-# shellcheck disable=SC2093
-exec /init --config-file="/tmp/clamd.conf"
+/init --config-file="/tmp/clamd.conf"
 
 exec "$@"
