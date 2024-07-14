d8fe931c56a3f38a04001ead91c84f296c4fe067
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-07-14 19:53:07 +0200

    add on start generating caddyfile
    Signed-off-by: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

---
+++ Containers/apache/caddyfile.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+
+
+
+
+
+function loop {
+  readarray -t sorted < <(echo "$3" | tr "$2" '\n' | sort -r)
+  for i in "${sorted[@]}"; do
+    "template_loop_$1" "$i"
+  done
+
+}
+
+function template_nextcloud_route() {
+  cat << CADDY
+
+    route {
+        header Strict-Transport-Security max-age=31536000;
+        reverse_proxy localhost:8000
+    }
+    redir /.well-known/carddav /remote.php/dav/ 301
+    redir /.well-known/caldav /remote.php/dav/ 301
+
+    tls {
+        issuer acme {
+            disable_http_challenge
+        }
+    }
+CADDY
+}
+
+
+
+function template_loop_route {
+  IFS=',' read -ra array <<< "$1"
+  ROUTE="${array[0]}"
+  URI_STRIP_PREFIX="${array[1]}"
+  TARGET="${array[2]}"
+
+  cat << CADDY
+
+    route $(test -z "$ROUTE" || echo "$ROUTE/* "){
+        $([ "$URI_STRIP_PREFIX" == "1" ] && echo "uri strip_prefix $ROUTE")
+        reverse_proxy $TARGET
+    }
+CADDY
+}
+
+
+
+function template_loop_subdomain {
+  IFS='|' read -ra array <<< "$1"
+  SUBDOMAIN="${array[0]}"
+  ROUTES="${array[1]}"
+
+  cat << CADDY
+
+$(echo "$TRUSTED_DOMAINS" | tr ',' '\n' | sed "s/.*/$PROTOCOL:\/\/$SUBDOMAIN&:$APACHE_PORT/" | sed '$ ! s/$/,/') {
+    header -Server
+    header -X-Powered-By
+$(loop route ';' "$ROUTES")
+$(test -z "$SUBDOMAIN" && template_nextcloud_route)
+}
+CADDY
+}
+
+function template_caddyfile {
+  if [ -z "$TRUSTED_DOMAINS" ]; then
+      IPv4_ADDRESS="private_ranges"
+      PROTOCOL="http"
+  else
+      IPv4_ADDRESS="$(dig "$APACHE_HOST" A +short +search | head -1 | sed 's|[0-9]\+$|0/16|')"
+      PROTOCOL="https"
+  fi
+
+cat << CADDY
+{
+    auto_https $(test -z "$TRUSTED_DOMAINS" && echo "off" || echo "disable_redirects")
+
+    storage file_system {
+        root /mnt/data/caddy
+    }
+
+    servers {
+        trusted_proxies static $IPv4_ADDRESS
+    }
+
+    log {
+        level ERROR
+    }
+}
+
+$(loop subdomain '@' "$CADDY_ROUTES")
+
+CADDY
+}
+
+template_caddyfile
--- Containers/apache/start.sh
+++ Containers/apache/start.sh
@@ -17,46 +17,20 @@ while ! nc -z "$NEXTCLOUD_HOST" 9000; do
     sleep 5
 done
 
-# Get ipv4-address of Apache
-# shellcheck disable=SC2153
-IPv4_ADDRESS="$(dig "$APACHE_HOST" A +short +search | head -1)"
-# Bring it in CIDR notation
-# shellcheck disable=SC2001
-IPv4_ADDRESS="$(echo "$IPv4_ADDRESS" | sed 's|[0-9]\+$|0/16|')"
-
 if [ -z "$APACHE_PORT" ]; then
     export APACHE_PORT="443"
 fi
 
-# Change variables in case of reverse proxies
-if [ "$APACHE_PORT" != '443' ]; then
-    export PROTOCOL="http"
-    export NC_DOMAIN=""
-else
-    export PROTOCOL="https"
+# Set trusted domains if not in reverse proxy mode
+if [ "$APACHE_PORT" == '443' ]; then
+  if [ -z "$ADDITIONAL_TRUSTED_DOMAIN" ]; then
+      export TRUSTED_DOMAINS="$NC_DOMAIN"
+  else
+      export TRUSTED_DOMAINS="$ADDITIONAL_TRUSTED_DOMAIN,$NC_DOMAIN"
+  fi
 fi
 
-# Change the auto_https in case of reverse proxies
-if [ "$APACHE_PORT" != '443' ]; then
-    CADDYFILE="$(sed 's|auto_https.*|auto_https off|' /Caddyfile)"
-else
-    CADDYFILE="$(sed 's|auto_https.*|auto_https disable_redirects|' /Caddyfile)"
-fi
-echo "$CADDYFILE" > /tmp/Caddyfile
-
-# Change the trusted_proxies in case of reverse proxies
-if [ "$APACHE_PORT" != '443' ]; then
-    CADDYFILE="$(sed 's|# trusted_proxies placeholder|trusted_proxies static private_ranges|' /tmp/Caddyfile)"
-else
-    CADDYFILE="$(sed "s|# trusted_proxies placeholder|trusted_proxies static $IPv4_ADDRESS|" /tmp/Caddyfile)"
-fi
-echo "$CADDYFILE" > /tmp/Caddyfile
-
-# Remove additional domain if not given
-if [ -z "$ADDITIONAL_TRUSTED_DOMAIN" ]; then
-    CADDYFILE="$(sed '/ADDITIONAL_TRUSTED_DOMAIN/d' /tmp/Caddyfile)"
-fi
-echo "$CADDYFILE" > /tmp/Caddyfile
+./caddyfile.sh > /tmp/Caddyfile
 
 # Fix the Caddyfile format
 caddy fmt --overwrite /tmp/Caddyfile
