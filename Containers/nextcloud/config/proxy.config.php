151b05ec014122311be55b26927b98dee6601e73
Simon L. <szaimen@e.mail.de>
2024-08-09 16:36:16 +0200

    nextcloud: config files: do not compare against false
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/nextcloud/config/proxy.config.php
+++ Containers/nextcloud/config/proxy.config.php
@@ -1,13 +1,13 @@
 <?php
-if (getenv('HTTP_PROXY') !== false) {
+if (getenv('HTTP_PROXY')) {
     $CONFIG['proxy'] = getenv('HTTP_PROXY');
 }
-if (getenv('HTTPS_PROXY') !== false) {
+if (getenv('HTTPS_PROXY')) {
     $CONFIG['proxy'] = getenv('HTTPS_PROXY');
 }
-if (getenv('PROXY_USER_PASSWORD') !== false) {
+if (getenv('PROXY_USER_PASSWORD')) {
     $CONFIG['proxyuserpwd'] = getenv('PROXY_USER_PASSWORD');
 }
-if (getenv('NO_PROXY') !== false) {
+if (getenv('NO_PROXY')) {
     $CONFIG['proxyexclude'] = explode(',', getenv('NO_PROXY'));
 }
