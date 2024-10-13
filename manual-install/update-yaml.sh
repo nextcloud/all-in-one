5f377247bc0b0480a4f640bc5517433cfbc2acb8
lll <2844835+flll@users.noreply.github.com>
2024-10-13 19:43:18 +0900

    Update manual-install/update-yaml.sh
    Signed-off-by: lll <2844835+flll@users.noreply.github.com>
    Co-Authored-By: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

--- manual-install/update-yaml.sh
+++ manual-install/update-yaml.sh
@@ -42,7 +42,7 @@ sed -i '/AIO_TOKEN/d' containers.yml
 sed -i '/AIO_URL/d' containers.yml
 sed -i '/DOCKER_SOCKET_PROXY_ENABLED/d' containers.yml
 sed -i '/ADDITIONAL_TRUSTED_PROXY/d' containers.yml
-sed -i 's/\(^[[:space:]]*- \([^=]*\)\)=%\2%/\1/' containers.yml
+sed -ie 's/\( *- \(\w*\)\)=\${\2\}/\1/' containers.yml
 
 TCP="$(grep -oP '[%A-Z0-9_]+/tcp' containers.yml | sort -u)"
 mapfile -t TCP <<< "$TCP"
