65419f580b18c946c2444d01d7c3abb4f5a42e93
Zoey <zoey@z0ey.de>
2025-02-27 15:23:46 +0100

    remove initial freshclam from start.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- Containers/clamav/start.sh
+++ Containers/clamav/start.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-if ! freshclam --foreground --stdout; then
-  exit 1
-fi
-
 sed "s|aio-placeholder|$MAX_SIZE|" /etc/clamav/clamd.conf > /tmp/clamd.conf
 
 freshclam --foreground --stdout --daemon & 
