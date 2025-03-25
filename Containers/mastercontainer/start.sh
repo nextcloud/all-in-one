13e9829a850ae27258b7e05f54849a3d33371cdb
Simon L. <szaimen@e.mail.de>
2025-03-25 11:01:42 +0100

    fix detail
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/mastercontainer/start.sh
+++ Containers/mastercontainer/start.sh
@@ -285,7 +285,7 @@ fi
 
 # Check if ghcr.io is reachable
 # Solves issues like https://github.com/nextcloud/all-in-one/discussions/5268
-if ! curl https://ghcr.io 2>&1; then
+if ! curl https://ghcr.io &>/dev/null; then
     print_red "Could not reach https://ghcr.io."
     echo "Most likely is something blocking access to it."
     echo "You should be able to fix this by using https://github.com/nextcloud/all-in-one/tree/main/manual-install"
