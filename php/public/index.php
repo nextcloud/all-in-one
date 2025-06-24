a54b89eabf13265209404349e9a850b2c5e2c83b
Alan Savage <asavage@ifrit.internal>
2025-06-24 09:26:16 -0700

    Add support for no_pull_container_ids param
    This allows easier local testing of the containers because you can
    build locally with the correct label then start the containers again.
    
    For example:
    1. Stop the containers via the AIO admin UI
    2. Build apache with `git push && docker buildx build -t ghcr.io/nextcloud-releases/aio-apache:beta --build-arg AIO_GIT_URL="https://github.com/asavageiv/all-in-one.git" --build-arg AIO_GIT_BRANCH="$(git branch --show-current)" --load --no-cache Containers/apache/`
    3. Go to the admin UI with no_pull_container_ids set, like this: `/containers?no_pull_container_ids=nextcloud-aio-apache`
    4. Push start containers
    
    Now you will be running with the locally built apache image.
    
    You can adjust the tag on the docker build command for :beta or :latest
    as needed.
    
    Signed-off-by: Alan Savage <asavage@ifrit.internal>

--- php/public/index.php
+++ php/public/index.php
@@ -86,6 +86,10 @@ $app->get('/containers', function (Request $request, Response $response, array $
     // Check if bypass_mastercontainer_update is provided on the URL, a special developer mode to bypass a mastercontainer update and use local image.
     $params = $request->getQueryParams();
     $bypass_mastercontainer_update = isset($params['bypass_mastercontainer_update']);
+    $no_pull_container_ids = '';
+    if (isset($params['no_pull_container_ids'])) {
+        $no_pull_container_ids = $params['no_pull_container_ids'];
+    }
 
     return $view->render($response, 'containers.twig', [
         'domain' => $configurationManager->GetDomain(),
@@ -136,6 +140,7 @@ $app->get('/containers', function (Request $request, Response $response, array $
         'is_whiteboard_enabled' => $configurationManager->isWhiteboardEnabled(),
         'community_containers' => $configurationManager->listAvailableCommunityContainers(),
         'community_containers_enabled' => $configurationManager->GetEnabledCommunityContainers(),
+        'no_pull_container_ids' => $no_pull_container_ids,
     ]);
 })->setName('profile');
 $app->get('/login', function (Request $request, Response $response, array $args) use ($container) {
