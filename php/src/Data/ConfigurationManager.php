983e523bde3d24dc89de36185653c17f0d831bf5
Simon L. <szaimen@e.mail.de>
2025-07-03 11:44:50 +0200

    make the check for backup dir and datadir fail proof
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- php/src/Data/ConfigurationManager.php
+++ php/src/Data/ConfigurationManager.php
@@ -487,7 +487,7 @@ class ConfigurationManager
                 throw new InvalidSettingConfigurationException("The path must start with '/', and must not end with '/'! Another option is to use the docker volume name 'nextcloud_aio_backupdir'.");
             }
 
-            if (str_starts_with($location, rtrim($this->GetNextcloudDatadirMount(), '/'))) {
+            if (str_starts_with($location . '/', rtrim($this->GetNextcloudDatadirMount(), '/') . '/')) {
                 throw new InvalidSettingConfigurationException("The path must not be a children of or equal to NEXTCLOUD_DATADIR, which is currently set to " . $this->GetNextcloudDatadirMount());
             }
 
