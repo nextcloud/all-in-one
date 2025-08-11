10b1ec5c6697d28ad0530e775ee423ada8643e49
Simon L. <szaimen@e.mail.de>
2025-08-11 10:36:52 +0200

    clamav: print out version during startup of container
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/clamav/start.sh
+++ Containers/clamav/start.sh
@@ -2,6 +2,9 @@
 
 sed "s|aio-placeholder|$MAX_SIZE|" /etc/clamav/clamd.conf > /tmp/clamd.conf
 
+# Print out clamav version for compliance reasons
+clamscan --version
+
 echo "Clamav started"
 
 exec "$@"
