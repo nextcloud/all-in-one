5137d1ce6b58660bbd45d065ae9073153fd9b34b
Simon L. <szaimen@e.mail.de>
2025-09-02 15:12:59 +0200

    collabora: allow to use enterprise container image with support key
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- php/src/Data/ConfigurationManager.php
+++ php/src/Data/ConfigurationManager.php
@@ -979,6 +979,13 @@ class ConfigurationManager
         return $config['collabora_additional_options'];
     }
 
+    public function isCollaboraSubscriptionEnabled() : bool {
+        if (str_contains($this->GetAdditionalCollaboraOptions(), '--o:support_key=')) {
+            return true;
+        }
+        return false;
+    }
+
     public function DeleteAdditionalCollaboraOptions() : void {
         $config = $this->GetConfig();
         $config['collabora_additional_options'] = '';
