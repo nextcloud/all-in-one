3eefa929bfbbe68e027cd23a9f562f40f17237a3
Lorenzo Moscati <lorenzo@moscati.page>
2025-10-14 15:59:28 +0200

    Added suggested changes
    Signed-off-by: Lorenzo Moscati <lorenzo@moscati.page>

--- php/public/base_path.js
+++ php/public/base_path.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("base_path") && (document.getElementById("base_path").value = window.location.pathname.slice(0, -11));
+document.addEventListener("DOMContentLoaded", function() {
+    basePath = document.getElementById("base_path")
+    if (basePath) {
+        // Remove '/containers' from the end of the path, to get the base path only
+        basePath.value = window.location.pathname.slice(0, -11);
+    }
 });
