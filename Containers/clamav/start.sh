cbe91447bf66d5ecac22ea934c5f7a2c592700d1
ruben <ruben@winterrific.net>
2025-10-14 19:57:01 +0200

    WIP: add clamav milter to nextcloud-aio-clamav container
    This contains installing and configuring the clamav-milter process and then starting it with supervisord.
    
    TODO: start milter only when the stalwart community container is activated

--- Containers/clamav/start.sh
+++ Containers/clamav/start.sh
@@ -2,6 +2,10 @@
 
 sed "s|aio-placeholder|$MAX_SIZE|" /etc/clamav/clamd.conf > /tmp/clamd.conf
 
+if [ "${STALWART}" ]; then
+  cp /etc/clamav/clamav-milter.conf /tmp/clamv-milter-conf
+fi
+
 # Print out clamav version for compliance reasons
 clamscan --version
 
