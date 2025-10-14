28f7d3571ca69fb408fe1f8f173b1d3ac0c8f9a6
Ruben D. <ruben@winterrific.net>
2025-10-14 20:59:42 +0200

    Add clamav milter packaged, configure it and use supervisord to start it up.
    TODO: start milter only if community container stalwart is used.
    
    Signed-off-by: Ruben D. <ruben@winterrific.net>

--- Containers/clamav/start.sh
+++ Containers/clamav/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed "s|aio-placeholder|$MAX_SIZE|" /etc/clamav/clamd.conf > /tmp/clamd.conf
+cp /etc/clamav/clamav-milter.conf /tmp/clamv-milter-conf
 
 # Print out clamav version for compliance reasons
 clamscan --version
