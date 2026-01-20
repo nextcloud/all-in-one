a18ef97ab411cd2acfff8cd25ddec25b9acef723
Oleksander Piskun <oleksandr2088@icloud.com>
2026-01-20 15:42:06 +0200

    feat(app-api): add HaRP container
    Signed-off-by: Oleksander Piskun <oleksandr2088@icloud.com>
    Signed-off-by: bigcat88 <bigcat88@icloud.com>

---
+++ php/public/disable-harp.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // HaRP
+    let harp = document.getElementById("harp");
+    if (harp) {
+        harp.disabled = true;
+    }
+});
