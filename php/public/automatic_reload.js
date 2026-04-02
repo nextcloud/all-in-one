7a9bab57766d66e40b9cb891532f195ea25aa55f
Simon L. <szaimen@e.mail.de>
2026-04-02 14:06:39 +0200

    aio-interface: fix page not automatic reloading after container starting progress spinner
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- php/public/automatic_reload.js
+++ php/public/automatic_reload.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+window.addEventListener("load", function(event) {
     if (document.hasFocus()) {
         // hide reload button if the site reloads automatically
         let list = document.getElementsByClassName("reload button");
