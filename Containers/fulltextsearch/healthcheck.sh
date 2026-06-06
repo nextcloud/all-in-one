6cbf5eeefb93a3417c7956e5e1783d95beeb114c
Simon L. <szaimen@e.mail.de>
2026-06-06 22:24:52 +0200

    fulltextsearch: fix healthcheck
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/fulltextsearch/healthcheck.sh
+++ Containers/fulltextsearch/healthcheck.sh
@@ -4,4 +4,4 @@ if [ "$AIO_LOG_LEVEL" = 'debug' ]; then
     set -x
 fi
 
-curl -fs -u "elastic:$FULLTEXTSEARCH_PASSWORD" "http://127.0.0.1:9200/_cluster/health?filter_path=status" | grep -qE '"status":"(green|yellow)"' || exit 1
+curl -fs -u "elastic:$ELASTIC_PASSWORD" "http://127.0.0.1:9200/_cluster/health?filter_path=status" | grep -qE '"status":"(green|yellow)"' || exit 1
