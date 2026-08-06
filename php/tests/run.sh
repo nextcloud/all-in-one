2977df67e850cb780be478f1823a58e3e8daf4d2
Simon L. <szaimen@e.mail.de>
2026-08-06 12:17:50 +0200

    try to fix playwright test
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- php/tests/run.sh
+++ php/tests/run.sh
@@ -29,6 +29,9 @@ run_tests() {
     # Clean up old containers and volumes
     $DOCO --profile $profile down -v --remove-orphans
     docker container rm --force nextcloud-aio-{mastercontainer,apache,notify-push,nextcloud,redis,database,domaincheck,whiteboard,imaginary,talk,collabora,borgbackup} > /dev/null 2>&1
+    # Wait a few seconds so that the containers are correctly removed
+    sleep 10
+    # Then remove the volumes
     docker volume rm nextcloud_aio_{mastercontainer,apache,database,database_dump,nextcloud,nextcloud_data,redis,backup_cache,elasticsearch} > /dev/null 2>&1
 
     echo -e "\n 📣  Running playwright tests for ${TESTS_FILE} with SKIP_DOMAIN_VALIDATION=$SKIP_DOMAIN_VALIDATION and profile '$profile'\n"
