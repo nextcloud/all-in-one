0f666d2d4b27c2b3bc283d9eeeaab25efdd6cd0f
Simon L. <szaimen@e.mail.de>
2026-08-25 13:46:40 +0200

    change Caddy ACME profile to shortlived
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/nextcloud/entrypoint.sh
+++ Containers/nextcloud/entrypoint.sh
@@ -947,6 +947,8 @@ if [ "$TALK_ENABLED" = 'yes' ]; then
     elif [ "$SKIP_UPDATE" != 1 ]; then
         php /var/www/html/occ app:update spreed
     fi
+    # We auto-renew the certs via caddy so the outdated cert warning should not be displayed
+    php /var/www/html/occ config:app:set certificate_expiration_days --type int --value="0"
     # Add turn server
     # shellcheck disable=SC2153
     if ! php /var/www/html/occ talk:turn:list --output="plain" | grep server | grep -q " $TURN_DOMAIN:$TALK_PORT"; then
