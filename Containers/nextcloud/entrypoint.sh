3e3ce552f0f7ebdf7a87abb7a46b87f1a4a334fd
Simon L. <szaimen@e.mail.de>
2026-08-26 11:24:15 +0200

    Update Containers/nextcloud/entrypoint.sh
    Co-authored-by: Zoey <zoey@z0ey.de>
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/nextcloud/entrypoint.sh
+++ Containers/nextcloud/entrypoint.sh
@@ -948,7 +948,7 @@ if [ "$TALK_ENABLED" = 'yes' ]; then
         php /var/www/html/occ app:update spreed
     fi
     # We auto-renew the certs via caddy so the outdated cert warning should not be displayed
-    php /var/www/html/occ config:app:set certificate_expiration_days --type int --value="0"
+    php /var/www/html/occ config:app:set spreed certificate_expiration_days --type=integer --value="2"
     # Add turn server
     # shellcheck disable=SC2153
     if ! php /var/www/html/occ talk:turn:list --output="plain" | grep server | grep -q " $TURN_DOMAIN:$TALK_PORT"; then
