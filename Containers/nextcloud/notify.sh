d47a22bcbc1e816110cd11bb5db9a65180d22f18
Simon L. <szaimen@e.mail.de>
2026-09-21 10:43:35 +0200

    nextcloud: optimize notify scripts
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- Containers/nextcloud/notify.sh
+++ Containers/nextcloud/notify.sh
@@ -23,17 +23,16 @@ if [ "$("${COMMAND[@]}" config:app:get notifications enabled)" = "no" ]; then
 fi
 
 echo "Posting notifications to users that are admins..."
-NC_USERS=$("${COMMAND[@]}" user:list | sed 's|^  - ||g' | sed 's|:.*||')
-mapfile -t NC_USERS <<< "$NC_USERS"
-for user in "${NC_USERS[@]}"
-do
-    if "${COMMAND[@]}" user:info "$user" | cut -d "-" -f2 | grep -x -q " admin"
-    then
-        NC_ADMIN_USER+=("$user")
-    fi
-done
+# Listing the admin group members with one occ call is much faster than running
+# 'occ user:info' per user and is not limited to the first 500 users.
+NC_ADMIN_USERS=$("${COMMAND[@]}" group:list admin | sed -n 's|^    - ||p')
+if [ -z "$NC_ADMIN_USERS" ]; then
+    echo "Could not find any admin user to post notifications to."
+    exit 1
+fi
+mapfile -t NC_ADMIN_USERS <<< "$NC_ADMIN_USERS"
 
-for admin in "${NC_ADMIN_USER[@]}"
+for admin in "${NC_ADMIN_USERS[@]}"
 do
     echo "Posting '$SUBJECT' to: $admin"
     "${COMMAND[@]}" notification:generate "$admin" "$NC_DOMAIN: $SUBJECT" -l "$MESSAGE" --object-type='update' --object-id="$OBJECT_ID"
